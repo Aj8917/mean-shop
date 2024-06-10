d4757e1a4951b405e650b7c3bd531d9f91f9fe9f
Aj8917 <ajinkyaabhosale@gmail.com>
2024-06-10 17:00:43 +0530

    authorization with JWT adn cookie

--- backend/server.js
+++ backend/server.js
@@ -5,12 +5,17 @@ import connectDB from './config/db.js'
 import productRoutes from './routes/productRoutes.js'
 import userRoutes from './routes/userRoutes.js'
 import { notFound,errorHandler } from './middleware/errorMiddleware.js';
+import cookieParser from 'cookie-parser';
 
 const port = process.env.PORT || 5000;
 dotenv.config();
 const app = express();
 
 app.use(express.json());
+app.use(express.urlencoded({extended :true}));
+
+//cookie parser middleware 
+app.use(cookieParser());
 
 app.use('/api/products', productRoutes);
 app.use('/api/users',userRoutes);
